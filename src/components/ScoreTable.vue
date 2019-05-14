1e3a44fa2ffaa2ab3670535ea56023c57d79175b
Ragura <ragura@gmail.com>
2019-05-14 14:41:59 +0200

    Support for Colors in ScoreTable

--- src/components/ScoreTable.vue
+++ src/components/ScoreTable.vue
@@ -1,31 +1,48 @@
 <template>
-  <table class="w-full">
+  <table class="w-full border-separate">
     <thead>
-      <tr>
+      <tr class="border-b border-gray-600">
         <th class="text-left">Leerling</th>
-        <th class="text-center" v-for="n of evaluatie.amount" :key="punt + n">
-          <div class="w-full flex items-center vertical leading-none">
-            Toets {{ n }}
-          </div>
+        <th class="pb-2" v-for="n of evaluatie.amount" :key="punt + n">
+          <div class="mx-auto vertical leading-none">Eval. {{ n }}</div>
         </th>
         <th>Gemiddelde</th>
       </tr>
     </thead>
     <tbody>
       <tr
+        class=""
         v-for="leerling of Object.values(leerlingen)"
         :key="`${leerling.id}-${punt}`"
       >
-        <td>{{ leerling.voornaam }} {{ leerling.familienaam }}</td>
-        <td class="text-center" v-for="n of evaluatie.amount" :key="punt + n">
-          {{ leerling.punten[punt][n - 1] }}
+        <td class="">{{ leerling.voornaam }} {{ leerling.familienaam }}</td>
+        <td
+          class="text-center h-10"
+          v-for="n of evaluatie.amount"
+          :key="punt + n"
+        >
+          <table-color
+            v-if="evaluatie.type === 'color'"
+            :value="leerling.punten[punt] && leerling.punten[punt][n - 1]"
+            @change="updateColorArray(n - 1, leerling.id, punt, $event)"
+          />
+          <p v-else-if="evaluatie.type === 'points'">
+            {{ leerling.punten[punt] ? leerling.punten[punt][n - 1] : "" }}
+          </p>
         </td>
-        <td>
-          {{
-            evaluatie.type === "color"
-              ? leerling.punten[`${punt}-final`]
-              : gemiddelde(leerling.punten[punt])
-          }}
+        <td class="h-10 text-center">
+          <table-color
+            v-if="evaluatie.type === 'color'"
+            :value="
+              leerling.punten[`${punt}-final`]
+                ? leerling.punten[`${punt}-final`]
+                : 0
+            "
+            @change="updateColorValue(leerling.id, `${punt}-final`, $event)"
+          />
+          <p v-else-if="evaluatie.type === 'points'">
+            {{ average(leerling.punten[punt]) }}
+          </p>
         </td>
       </tr>
     </tbody>
@@ -34,8 +51,12 @@
 
 <script>
 import { mapState, mapActions } from "vuex";
+import TableColor from "@/components/TableColor.vue";
 export default {
   name: "ScoreTable",
+  components: {
+    TableColor
+  },
   props: {
     punt: {
       type: String,
@@ -49,7 +70,39 @@ export default {
     }
   },
   methods: {
-    ...mapActions(["updatePunten"])
+    ...mapActions(["updatePunten"]),
+    average(arrayPunten) {
+      const punten = arrayPunten.map((punt, index) => {
+        return punt * (10 / this.evaluaties[this.punt].maximums[index]);
+      });
+      const total = punten.reduce((total, value) => {
+        return (total += value);
+      }, 0);
+      return (total / punten.length).toFixed(1);
+    },
+    updateColorArray(index, leerlingId, evaluatieId, value) {
+      const punten = this.leerlingen[leerlingId].punten[evaluatieId];
+      let arrayColors;
+      if (punten) {
+        arrayColors = [...punten];
+      } else {
+        arrayColors = Array(this.evaluaties[this.punt].amount);
+      }
+
+      arrayColors[index] = value;
+      this.updatePunten({
+        leerlingId,
+        evaluatieId,
+        value: arrayColors
+      });
+    },
+    updateColorValue(leerlingId, evaluatieId, value) {
+      this.updatePunten({
+        leerlingId,
+        evaluatieId,
+        value
+      });
+    }
   }
 };
 </script>
@@ -58,5 +111,9 @@ export default {
 .vertical {
   writing-mode: vertical-rl;
   transform: rotate(180deg);
+}
+
+table {
+  border-spacing: 0.5rem;
 }
 </style>
---
+++ src/components/TableColor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    @mouseover="displayColors = true"
+    @mouseleave="displayColors = false"
+    class="h-full w-full relative"
+    :style="{ backgroundColor: colors[value] }"
+  >
+    <div
+      v-show="displayColors"
+      class="w-full h-full absolute cursor-pointer bottom-0 z-50"
+    >
+      <div
+        @click="$emit('change', index)"
+        class="w-full h-full"
+        v-for="(color, index) of colors"
+        :key="color"
+        :style="{ backgroundColor: color }"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      colors: ["white", "red", "yellow", "lightgreen", "lightblue"],
+      displayColors: false
+    };
+  }
+};
+</script>
+
+<style></style>
