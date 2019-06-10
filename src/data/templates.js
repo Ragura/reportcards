ca754a3802b279f08a1c8d120cad42409ac40296
Ragura <ragura@gmail.com>
2019-06-11 00:39:09 +0200

    Added temporary dupes of all possible templates

--- src/data/templates.js
+++ src/data/templates.js
@@ -1,3 +1,21 @@
+import L1_1 from "@/data/L1_1.json";
+import L2_1 from "@/data/L2_1.json";
+import L3_1 from "@/data/L3_1.json";
+import L4_1 from "@/data/L4_1.json";
+import L5_1 from "@/data/L5_1.json";
+import L6_1 from "@/data/L6_1.json";
+import L1_2 from "@/data/L1_2.json";
+import L2_2 from "@/data/L2_2.json";
+import L3_2 from "@/data/L3_2.json";
+import L4_2 from "@/data/L4_2.json";
+import L5_2 from "@/data/L5_2.json";
+import L6_2 from "@/data/L6_2.json";
+import L1_3 from "@/data/L1_3.json";
+import L2_3 from "@/data/L2_3.json";
+import L3_3 from "@/data/L3_3.json";
+import L4_3 from "@/data/L4_3.json";
+import L5_3 from "@/data/L5_3.json";
+import L6_3 from "@/data/L6_3.json";
 import L1_4 from "@/data/L1_4.json";
 import L2_4 from "@/data/L2_4.json";
 import L3_4 from "@/data/L3_4.json";
@@ -6,6 +24,24 @@ import L5_4 from "@/data/L5_4.json";
 import L6_4 from "@/data/L6_4.json";
 
 export default {
+  L1_1,
+  L2_1,
+  L3_1,
+  L4_1,
+  L5_1,
+  L6_1,
+  L1_2,
+  L2_2,
+  L3_2,
+  L4_2,
+  L5_2,
+  L6_2,
+  L1_3,
+  L2_3,
+  L3_3,
+  L4_3,
+  L5_3,
+  L6_3,
   L1_4,
   L2_4,
   L3_4,
